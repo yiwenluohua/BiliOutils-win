1514dc4de615fdfd3457a5b28ae379fad22eaa92
yiwenluohua <81276350+yiwenluohua@users.noreply.github.com>
2024-03-01 17:54:41 +0800

    Update and rename 新建 文本文档.bat to biliOutil-win.bat

--- biliOutil-win.bat
+++ biliOutil-win.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
  
 rem 将需要运行的cmd命令写入到一个文本文件中（每条命令占据一行）
 (
-	rem 安装/更新biliOutils
+    rem 安装/更新biliOutils
     echo npm install -g bilioutils
     rem 执行在当前文件目录下的config.json配置文件，如需执行多个文件，请自行根据官网修改下方指令
     echo bilioutils -c ./config.json
